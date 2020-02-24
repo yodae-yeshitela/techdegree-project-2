e98290f61536c36e1d6391af8eccba5eb8a6d1ad
yodae.yeshitela <yodae.yeshitela@gmail.com>
2020-02-23 23:15:33 -0800

    Implement all features

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -2,84 +2,103 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-const students = document.getElementsByClassName("student-item");
 const page = document.getElementsByClassName("page")[0];
-const pages = Math.ceil(students.length / 10);
+const header = document.getElementsByClassName("page-header")[0];
+const students = document.getElementsByClassName("student-item");
+var view = "";
 var currentPage = 1;
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-function showPage(page){
-   console.log(page);
-   for( let i = 0; i < students.length; i++){
-      if(i>= (page - 1) * 10 && i<page*10){
-         students[i].style.display = "block";
-      }
-      else{
-         students[i].style.display = "none";
-      }
-   }
+function reset() {
+  for (let student of students) {
+    student.style.display = "none";
+  }
+  if (page.getElementsByClassName("error")[0])
+    page.getElementsByClassName("error")[0].remove();
+  if (document.getElementsByClassName("pagination")[0])
+    document.getElementsByClassName("pagination")[0].remove();
 }
 
+function showPage(page, students) {
+  for (let i = 0; i < students.length; i++) {
+    if (i >= (page - 1) * 10 && i < page * 10) {
+      students[i].style.display = "block";
+    } else {
+      students[i].style.display = "none";
+    }
+  }
+}
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-function appendPageLinks(pages){
-   let links = document.createElement("ul","id=1");
-   links.className = "pagination";
-   for(let i = 1 ; i <= 6; i++ ){
-      let li = document.createElement('li');
-      let a = document.createElement('a')
-      a.innerText = i;
-      a.id = i;
-      if(i == currentPage){
-         a.className = "active";
+function appendPageLinks(students) {
+  let pages = Math.ceil(students.length / 10);
+  let links = document.createElement("ul");
+  links.className = "pagination";
+  for (let i = 1; i <= pages; i++) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.innerText = i;
+    a.id = i;
+    if (i == currentPage) {
+      a.className = "active";
+    }
+    a.onclick = event => handleNavigate(event);
+    li.append(a);
+    links.append(li);
+  }
+  page.appendChild(links);
+}
+
+function handleNavigate(event) {
+  showPage(event.target.id, students);
+  event.target.className = "active";
+  if (document.getElementById(currentPage))
+    document.getElementById(currentPage).classList.remove("active");
+  currentPage = event.target.id;
+}
+
+function addSearchBox() {
+  let input = document.createElement("input");
+  input.placeholder = "Search for students...";
+  input.onkeyup = event => search(event.target.value);
+  let button = document.createElement("button");
+  button.innerText = "Search";
+  button.onclick = event => search(event.target.previousElementSibling.value);
+
+  let div = document.createElement("div");
+  div.className = "student-search";
+
+  div.appendChild(input);
+  div.appendChild(button);
+  header.appendChild(div);
+}
+
+function search(searchText) {
+  let searchResults = [];
+  reset();
+  if (searchText != "") {
+    for (let i = 0; i < students.length; i++) {
+      if (
+        students[i].getElementsByTagName("h3")[0].innerText.includes(searchText)
+      ) {
+        searchResults.push(students[i]);
       }
-      a.onclick = (e) => handleClick(e);
-      li.append(a);
-      links.append(li);
-   }
-   page.appendChild(links);
+    }
+    if (students.length != 0) {
+      showPage(1, searchResults);
+      appendPageLinks(searchResults);
+    } else {
+      let error = document.createElement("div");
+      error.className = "error";
+      error.innerText = "No results";
+      page.appendChild(error);
+    }
+  } else {
+    students = document.getElementsByClassName("student-item");
+    showPage(currentPage, students);
+    appendPageLinks(students);
+  }
 }
 
-function handleClick(event){
-   showPage(event.target.id);
-   event.target.className = "active";
-   document.getElementById(currentPage).classList.remove("active");
-   currentPage = event.target.id;
-}
-showPage(currentPage);
-appendPageLinks(pages);
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+showPage(1, students);
+appendPageLinks(students);
+addSearchBox();
